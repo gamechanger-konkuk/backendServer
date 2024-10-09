85580c8b22577e7a9ad3fded4d2b535bf5a00caa
sangjoon <jr1504@konkuk.ac.kr>
2024-10-09 11:53:31 +0900

    feat: 티셔츠 이름 수정하는 sql 구문 추가

--- src/main/java/com/gamechanger/repository/ClothesRepository.java
+++ src/main/java/com/gamechanger/repository/ClothesRepository.java
@@ -2,10 +2,13 @@ package com.gamechanger.repository;
 
 import com.gamechanger.domain.Clothes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ClothesRepository extends JpaRepository<Clothes, String> {
     Optional<Clothes> findByClothesName(String clothesName);
     void deleteByClothesName(String clothesName);
+    @Query("UPDATE Clothes c SET c.clothesName = :newClothesName WHERE c.clothesName = :oldClothesName")
+    Clothes changeClothesName(String oldClothesName, String newClothesName);
 }
