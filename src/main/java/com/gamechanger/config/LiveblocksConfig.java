f6656378087ba86e798b035e6a90e786a58745bd
sangjoon <jr1504@konkuk.ac.kr>
2024-10-09 11:46:58 +0900

    chore: 서비스 어노테이션 대신 빈 등록으로 수정

--- src/main/java/com/gamechanger/config/LiveblocksConfig.java
+++ src/main/java/com/gamechanger/config/LiveblocksConfig.java
@@ -1,15 +1,30 @@
 package com.gamechanger.config;
 
+import com.gamechanger.client.LiveblocksClient;
+import com.gamechanger.service.LiveblocksService;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LiveblocksConfig {
+    private final LiveblocksClient liveblocksClient;
+
     @Value("${spring.liveBlocks.secretKey}")
     private String secretKey;
+
+    @Autowired
+    public LiveblocksConfig(LiveblocksClient liveblocksClient) {
+        this.liveblocksClient = liveblocksClient;
+    }
+
+    @Bean
+    public LiveblocksService liveblocksService() {
+        return new LiveblocksService(liveblocksClient);
+    }
 
     @Bean
     public RequestInterceptor requestInterceptor() {
