492418c48487438440e9cb5654a6eba3f798974c
sangjoon <jr1504@konkuk.ac.kr>
2024-10-09 21:23:35 +0900

    feat: 배경 제거 시 전송하는 이미지 타입 MultipartFile로 변경

--- src/main/java/com/gamechanger/client/AiClient.java
+++ src/main/java/com/gamechanger/client/AiClient.java
@@ -4,6 +4,7 @@ import com.gamechanger.dto.ai.image.CreateImageRequestByPromptToAi;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "aiClient", url = "${ai.server.url}")
 public interface AiClient {
@@ -15,7 +16,7 @@ public interface AiClient {
 //    byte[] generateImageByImage(@RequestBody CreateImageRequestByImageDto createImageRequestByImageDto);
 
     @PostMapping(value = "/remove-background", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    byte[] removeImageBackground(@RequestPart("file") byte[] image);
+    byte[] removeImageBackground(@RequestPart("file") MultipartFile image);
 
 }
 
