ecd713d981bcd5c89a7751ce72828bb282f83bf4
sangjoon <jr1504@konkuk.ac.kr>
2024-10-17 02:09:21 +0900

    fix: 유저 role 반환 함수 추가

--- src/main/java/com/gamechanger/domain/User.java
+++ src/main/java/com/gamechanger/domain/User.java
@@ -45,4 +45,7 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private List<Clothes> clothesList = new ArrayList<>();
 
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
