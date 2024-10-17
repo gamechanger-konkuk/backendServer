4a86123d01f89a21bb05a30ebcfa38d90e4d6067
sangjoon <jr1504@konkuk.ac.kr>
2024-10-18 02:24:42 +0900

    feat: 티셔츠, 이미지에 대한 비즈니스 로직을 사용자 중심으로 수정
    - "사용자의" 티셔츠 CRUD 되도록 구현
    - "사용자의" 티셔츠에 이미지 삽입되도록 구현

--- src/main/java/com/gamechanger/service/UserServiceImpl.java
+++ src/main/java/com/gamechanger/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gamechanger.service;
 
+import com.gamechanger.domain.Clothes;
+import com.gamechanger.domain.Image;
 import com.gamechanger.domain.User;
 import com.gamechanger.dto.user.JoinRequest;
 import com.gamechanger.dto.user.LoginRequest;
@@ -7,8 +9,13 @@ import com.gamechanger.repository.UserRepository;
 import com.gamechanger.util.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.parser.ParseException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -20,6 +27,7 @@ public class UserServiceImpl implements UserService {
     private final JwtTokenProvider jwtTokenProvider;
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    // User
     @Override
     public User join(JoinRequest joinRequest) {
         // 중복 id 존재 시 null 반환
@@ -41,7 +49,7 @@ public class UserServiceImpl implements UserService {
         if (!encoder.matches(loginRequest.getPassword(), user.getPassword())) {
             return null;
         }
-        return jwtTokenProvider.createToken(user.getLoginId(), user.getName());
+        return jwtTokenProvider.createToken(user.getLoginId(), user.getUserName());
     }
 
     @Override
@@ -52,5 +60,94 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean checkLoginIdDuplicate(String loginId) {
         return userRepository.existsByLoginId(loginId);
+    }
+
+    //Clothes
+    @Override
+    public Clothes createClothes(String loginId, String clothesName) throws ParseException {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        return clothesService.createClothes(user, clothesName);
+    }
+
+    @Override
+    public Clothes getClothes(String loginId, String clothesName) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        return user.getClothesByClothesName(clothesName);
+    }
+
+    @Override
+    public List<Clothes> getAllClothes(String loginId) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        return new ArrayList<>(user.getClothesList().values());
+    }
+
+    @Override
+    public Clothes saveClothes(String loginId, String clothesName) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        Clothes clothes = user.getClothesByClothesName(clothesName);
+        return clothesService.saveClothes(clothes.getSystemClothesId());
+    }
+
+    @Override
+    public void deleteClothes(String loginId, String clothesName) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return;
+        }
+        Clothes clothes = user.getClothesByClothesName(clothesName);
+        clothesService.deleteClothes(clothes.getSystemClothesId());
+    }
+
+    @Override
+    public Clothes changeClothesName(String loginId, String oldClothesName, String newClothesName) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        Clothes clothes = user.getClothesByClothesName(oldClothesName);
+        return clothesService.changeClothesName(clothes.getSystemClothesId(), newClothesName);
+    }
+
+    // Image
+    @Override
+    public Image uploadUserImage(String loginId, MultipartFile uploadImage, String clothesName) throws IOException {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        Clothes clothes = user.getClothesByClothesName(clothesName);
+        return clothesService.uploadUserImage(clothes.getSystemClothesId(), uploadImage, clothesName);
+    }
+
+    @Override
+    public Image createAiImageByPrompt(String loginId, String clothesName, String style, String prompt) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        Clothes clothes = user.getClothesByClothesName(clothesName);
+        return clothesService.createAiImageByPrompt(clothes.getSystemClothesId(), clothesName, style, prompt);
+    }
+
+    @Override
+    public Image removeImageBackground(String loginId, String clothesName, String fileUrl) {
+        User user = getUserByLoginId(loginId);
+        if (user == null) {
+            return null;
+        }
+        Clothes clothes = user.getClothesByClothesName(clothesName);
+        return clothesService.removeImageBackground(clothes.getSystemClothesId(), clothesName, fileUrl);
     }
 }
