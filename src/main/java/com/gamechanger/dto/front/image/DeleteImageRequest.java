b917c85040ec986224e0b33e9a7f4a9f207207a2
sangjoon <jr1504@konkuk.ac.kr>
2024-10-27 21:18:22 +0900

    fix: 이미지 삭제 요청에 @RequestParam 대신 @RequestBody로 변경

---
+++ src/main/java/com/gamechanger/dto/front/image/DeleteImageRequest.java
@@ -0,0 +1,10 @@
+package com.gamechanger.dto.front.image;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteImageRequest {
+    private String fileUrl;
+}
